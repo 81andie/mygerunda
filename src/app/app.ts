b376f6954c016e8f615f17efad4613bb5f5ee7df
81andie <133395577+81andie@users.noreply.github.com>
2026-06-20 21:21:48 +0200

    generando los geojson

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,12 @@ import { Carrussel } from "./components/carrussel/carrussel";
 import { Hero } from "./components/hero/hero";
 
 import { BentoGrid } from "./components/bento-grid/bento-grid";
+import { Cards } from "./components/cards/cards";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Carrussel, Hero, BentoGrid],
+  imports: [RouterOutlet, Navbar, Carrussel, Hero, BentoGrid, Cards, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
