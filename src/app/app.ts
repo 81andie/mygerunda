6f84910ffef5b232d676b9201476594cace7a86f
81andie <133395577+81andie@users.noreply.github.com>
2026-06-20 15:31:27 +0200

    bentogrid

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { initFlowbite } from 'flowbite';
 import { Navbar } from "./components/navbar/navbar";
 import { Carrussel } from "./components/carrussel/carrussel";
 import { Hero } from "./components/hero/hero";
-import { Cards } from "./components/cards/cards";
+
+import { BentoGrid } from "./components/bento-grid/bento-grid";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Carrussel, Hero, Cards],
+  imports: [RouterOutlet, Navbar, Carrussel, Hero, BentoGrid],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
